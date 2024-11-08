d13dd8364fbca2d2673d590f582d409fed497b54
Kilowat <kadantsev.sergey@gmail.com>
2024-11-08 22:38:26 +0300

    work

--- src/fermaScene.ts
+++ src/fermaScene.ts
@@ -1,8 +1,19 @@
 
-import { Animation, Engine, Random, Scene, Timer } from "excalibur";
+import { Actor, Animation, Engine, Random, Resource, Scene, Sprite, SpriteSheet, Timer, vec } from "excalibur";
+import { Resources } from "resources";
 
 export class FermaScene extends Scene {
     onInitialize(engine: Engine): void {
-
+        const actor = new Actor({ pos: vec(50, 50) });
+        const image = Resources.staticSpriteSheet;
+        const sprite = SpriteSheet.fromImageSourceWithSourceViews({
+            image,
+            sourceViews: [
+                { x: 1, y: 1, width: 71, height: 44 },
+                { x: 20, y: 0, width: 40, height: 50 },
+            ],
+        })
+        actor.graphics.use(sprite.getSprite(0, 0))
+        this.add(actor);
     }
 }
